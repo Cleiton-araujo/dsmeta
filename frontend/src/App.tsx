ba2357ee58e6ea0d015e169bc1d979443a5d9c3d
cleiton araujo <cleitonaraujo077@gmail.com>
2022-11-25 21:25:55 -0400

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header"
 import SalesCards from "./components/SalesCards"
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
